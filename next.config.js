3ecbcaeb5b0cab3ae5981e6cdbb753bdcfec0be6
Glenn Troncquo <glenntroncquo1@gmail.com>
2025-08-31 13:07:28 +0200

    allow cloudflare images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,26 @@ const nextConfig = {
   //   // Required: for next 13
   //   appDir: true
   // }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-8b9194a017634e559563eb04c14827ba.r2.dev",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    formats: ["image/webp", "image/avif"],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 module.exports = nextConfig;
